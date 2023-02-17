4255b9b464311042a4c29ba5e8636b4045058dfe
Frarif <freakinarif25@gmail.com>
2023-02-17 17:21:28 +0700

    change: div to form in home section

--- src/components/home-section.tsx
+++ src/components/home-section.tsx
@@ -9,16 +9,16 @@ export function HomeSection() {
         <p className="text-xl text-green-800 mt-5">
           We are here to make your nice travel journey
         </p>
-        <div className="flex mt-8 mb-64">
+        <form className="flex mt-8 mb-64" id="search">
           <input
             type="search"
             placeholder="Find your spot"
             className="pl-2.5 py-5 pr-80 bg-slate-100 rounded-l-lg focus:outline-none"
           />{" "}
-          <button className="py-2 px-7  bg-cyan-500 rounded-r-lg font-semibold text-white text-base  ">
+          <button className="py-2 px-7  bg-cyan-500 rounded-r-lg font-semibold text-white text-base  hover:bg-cyan-700  ">
             Search
           </button>
-        </div>
+        </form>
       </div>
     </section>
   );
