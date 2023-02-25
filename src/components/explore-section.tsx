aac5c0edded5f9a96da691679269c6d46913d44c
Muh Farkhan Syafii <zheninjas75@yahoo.co.id>
2023-02-26 06:33:56 +0700

    fix: change explore section title position

--- src/components/explore-section.tsx
+++ src/components/explore-section.tsx
@@ -5,7 +5,7 @@ import Card from "./card";
 export function ExploreSection(props: { destinations: ResourceDestinations }) {
   return (
     <div id="explore-section" className="container max-w-5xl sm:mx-auto">
-      <p className="my-8 text-3xl font-semibold text-green-800">
+      <p className="my-8 text-center text-3xl font-semibold text-green-800 md:text-left">
         Explore your spot
       </p>
       <div className="mb-8 grid grid-cols-1 gap-4 px-2 sm:grid-cols-2 md:grid-cols-4 md:px-0">
