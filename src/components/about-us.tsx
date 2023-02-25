e021a4c75b93a148b9c8188e7c4975399e78e080
Muh Farkhan Syafii <zheninjas75@yahoo.co.id>
2023-02-25 15:11:27 +0700

    refactor: add responsive to about us section

--- src/components/about-us.tsx
+++ src/components/about-us.tsx
@@ -1,27 +1,32 @@
+import { Link } from "react-router-dom";
+
 export function AboutUs() {
   return (
-    <section className="flex px-40 py-16 bg-teal-100">
+    <section className="flex flex-col md:flex-row px-4 md:px-40 py-16 bg-teal-100">
       <img
+        className="w-full object-cover"
         src="https://ik.imagekit.io/travelscopee/about-us-img.png?ik-sdk-version=javascript-1.4.3&updatedAt=1676904256683"
         alt="About Us"
       />
-      <div className="flex-col justify-start pl-2">
+      <div className="flex flex-col text-center md:text-left md:pl-8 mt-10 md:mt-0">
         <p className="font-semibold text-3xl text-green-800">About Us</p>
-        <p className="font-bold text-4xl text-cyan-500">
-          Most popular travel list in catamyst batch 2
+        <p className="font-bold text-4xl text-cyan-500 tracking-wider capitalize">
+          Most popular travel list
         </p>
-        <p className="font-medium text-xl text-green-800 mt-8 ">
+        <p className="font-medium text-xl text-green-800 mt-8 tracking-widest">
           We are a travel platform that aims to provide the best tourist spots
           for travelers in Indonesia. We provide a list of the best prices for
           travel so that you can adjust to the budget you have..
         </p>
-        <p className="font-medium text-xl text-green-800 mt-8 ">
+        <p className="font-medium text-xl text-green-800 mt-8 tracking-widest">
           We are committed to providing a high quality service and an
           unforgettable experience for each of our customers.
         </p>
-        <button className="bg-cyan-500 p-2.5 text-white mt-4 rounded-lg hover:bg-cyan-700">
-          Read More
-        </button>
+        <Link to={"/about-us"}>
+          <button className="bg-cyan-500 p-2.5 font-semibold text-white mt-4 rounded-lg hover:bg-cyan-700">
+            Read More
+          </button>
+        </Link>
       </div>
     </section>
   );
