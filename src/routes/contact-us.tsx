63e7ae9468a38cc4b11e8409726f239c170ebcd9
Rofiq Ahmad Mubarok <rofiqahmadm@gmail.com>
2023-02-25 19:51:14 +0700

    create contact-us header

--- src/routes/contact-us.tsx
+++ src/routes/contact-us.tsx
@@ -1,8 +1,8 @@
 export function ContactUsRoute() {
   return (
     <div id="explore-section" className="space-y-20">
-      <header className="bg-blue-400 py-40 flex justify-between items-center px-5">
-        <div>
+      <header className="bg-400 py-40 flex justify-between items-center px-5">
+        <div className="flex justify-between items-center max-w-5xl mx-auto">
           <h1>Partner with Southeast Asia’s Leading Travel Platform</h1>
           <p>
             With more than 50 million monthly active users in Asia-Pacific and
@@ -13,6 +13,7 @@ export function ContactUsRoute() {
             options below.
           </p>
         </div>
+        img
       </header>
     </div>
   );
