f171013c8d35d5396baed5ac34afbadc02e6d816
Frarif <freakinarif25@gmail.com>
2023-02-25 22:17:37 +0700

    refix: responsive about us section

--- src/components/about-us.tsx
+++ src/components/about-us.tsx
@@ -8,23 +8,23 @@ export function AboutUs() {
         src="https://ik.imagekit.io/travelscopee/about-us-img.png?ik-sdk-version=javascript-1.4.3&updatedAt=1676904256683"
         alt="About Us"
       />
-      <div className="mt-10 flex flex-col md:mt-0 md:pl-8">
-        <p className="text-3xl font-semibold text-green-800">About Us</p>
-        <p className="text-4xl font-bold capitalize text-cyan-500">
+      <div className="mt-10 flex-col md:mt-0 md:pl-8">
+        <p className="max-w-lg text-2xl font-semibold text-green-800 sm:text-3xl md:max-w-2xl md:text-4xl">About Us</p>
+        <p className="max-w-lg text-2xl font-semibold capitalize text-cyan-500 sm:text-3xl md:max-w-2xl md:text-4xl">
           Most popular travel list
         </p>
-        <p className="mt-8 text-xl font-medium text-green-800 ">
+        <p className="mt-6 text-base text-green-800 sm:text-lg md:text-xl">
           We are a travel platform that aims to provide the best tourist spots
           for travelers in Indonesia. We provide a list of the best prices for
           travel so that you can adjust to the budget you have..
         </p>
-        <p className="mt-8 text-xl font-medium text-green-800 ">
+        <p className="mt-6 mb-6 text-base text-green-800 sm:text-lg md:text-xl">
           We are committed to providing a high quality service and an
           unforgettable experience for each of our customers.
         </p>
         <Link
           to={"/about-us"}
-          className="mt-4 rounded-lg bg-cyan-500 p-2.5 font-semibold text-white hover:bg-cyan-700"
+          className="rounded-lg bg-cyan-500 p-2.5 text-base font-thin text-white hover:bg-cyan-700"
         >
           Read More
         </Link>
