189dee62f7ea155a4800159d901644da029014c5
Rofiq Ahmad Mubarok <rofiqahmadm@gmail.com>
2023-02-25 22:06:41 +0700

    fix contact us page

--- src/routes/contact-us.tsx
+++ src/routes/contact-us.tsx
@@ -17,9 +17,9 @@ export function ContactUsRoute() {
         </div>
       </header>
 
-      <section className="mx-auto flex max-w-full items-center justify-between bg-teal-100 py-2.5">
+      <section className="mx-auto flex max-w-full items-center justify-between bg-teal-100 py-10">
         <div className="max-w-lg space-y-4">
-          <h2 className="leading realxed max-w-sm pl-14 font-bold">
+          <h2 className="leading relaxed mx-auto max-w-sm pl-14 text-2xl font-bold">
             Customer Cervice
           </h2>
           <p className="max-w-sm pl-14">
