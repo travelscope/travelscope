f29d9adf9f5b4e691e5faff2b7e18e8eb6965eca
Rofiq Ahmad Mubarok <rofiqahmadm@gmail.com>
2023-02-25 20:22:40 +0700

    fix contact us header

--- src/routes/contact-us.tsx
+++ src/routes/contact-us.tsx
@@ -3,12 +3,14 @@ export function ContactUsRoute() {
     <div id="explore-section" className="space-y-20">
       <header className="contact-us-cover py-40 px-5">
         <div className="mx-auto max-w-5xl items-center space-y-5">
-          <h1>Partner with Southeast Asia’s Leading Travel Platform</h1>
-          <p>
+          <h1 className="max-w-sm space-y-2 text-2xl font-bold">
+            Partner with Southeast Asia’s Leading Travel Platform
+          </h1>
+          <p className="space-y2 text02xl max-w-sm">
             With more than 50 million monthly active users in Asia-Pacific and
             beyond, Traveloka is here to support the growth of your business.
           </p>
-          <p>
+          <p className="space-y2 text02xl max-w-sm">
             Choose the partnership that best suit your needs from the various
             options below.
           </p>
